3051bd73ecf03b736d1131875b0820e0659ff1ca
Fengxiaoxiao-001 <2655804186@qq.com>
2026-05-10 01:22:02 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,25 @@
+from .clip_offloader import (
+    VRAM_CLIP_Offloader
+)
+
+from .ModelAndLoraToModel import (
+    TrueModelMixerDictFuser
+)
+
+from .LoraStack import (
+    MultiLoRAStack
+)
+
+NODE_CLASS_MAPPINGS = {
+    "VRAM_CLIP_Offloader": VRAM_CLIP_Offloader,
+    "TrueModelMixerDictFuser": TrueModelMixerDictFuser,
+    "MultiLoRAStack": MultiLoRAStack
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "VRAM_CLIP_Offloader": "🔄 VRAM CLIP Offloader（CLIP 搬到 CPU/NPU）",
+    "TrueModelMixerDictFuser": "️【SDXL】 Model Mixer ",
+    "MultiLoRAStack": "【SDXL】多 LoRA 堆叠器"
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
